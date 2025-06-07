59f36fb5844ae117d04078db833e1620a2a2e070
unknown <thehex.sniffer@gmail.com>
2025-06-07 18:56:15 +0530

    Completed TODOs: - [ ]  connect using websocket to each available device     - [x]  create web socket server on each device     - [x]  connect to the web socket server of each device

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
 const startBroadcasting = require("./broadcaster");
 const startListening = require("./listener");
+const ws = require("ws");
+
 let discoveredDevices = new Set();
+const connectedClients = new Set(); // Tracks devices already connected via WebSocket
 
 startBroadcasting((socket) => {
 	startListening(socket, (msg, rinfo) => {
 		discoveredDevices.add(rinfo.address);
 		discoveredDevices.forEach((device) => {
 			console.log("discovered device: ", device);
+			if (connectedClients.has(device)) return;
+
+			// Initiate connection to discovered {device}'s websocket server
+			const wsClient = new ws(`ws://${device}:8080`);
+			wsClient.on("open", () => {
+				console.log("Connected to: ", device);
+				connectedClients.add(device);
+				console.table(connectedClients);
+			});
+
+			wsClient.on("message", (data) => {
+				console.log("data from server: ", data.toString("utf-8"));
+				wsClient.send(
+					Buffer.from("I am client, ready to recieve clips"),
+					(err) => {
+						if (err) {
+							console.log("Error: ", err);
+						}
+					}
+				);
+			});
+
+			wsClient.on("close", (code, reason) => {
+				console.log(
+					`closing the connection with ${device} due to  ${code}: ${reason}`
+				);
+			});
 		});
 	});
+	// Start websocket Server
+	websocketServer(() => {});
 });
+
+function websocketServer(callback) {
+	const server = new ws.WebSocket.Server({ port: 8080 });
+
+	server.on("connection", (socket, req) => {
+		socket.on("message", (data) => {
+			console.log("Message from client: ", data.toString("utf-8"));
+		});
+	});
+}
